14119f6d5f588970fe1abf5672027ddd29561a7b
bfrondall <46687790+bfrondall@users.noreply.github.com>
2021-02-24 09:56:46 -0700

    Final Copt of Assignment 1

--- js/content.js
+++ js/content.js
@@ -1,11 +1,26 @@
 console.log("extension is running!")
 
-$("header").after("hello from the extension")
+$("body").on("click", function(){
+	$("body").addClass("noise");
+	$("h2").addClass("shakytext");
+	$("p").addClass("shakytext");
+	$("btn").addClass("buttonshake");
 
-// TRY: updating the text content of an element or set of elements on one or more pages
-$(".hero-header .brand-header span").text("I've got big news")
-// TRY: adding a click event listener to an element that changes the background of that element to a new color
-$(".hero-header").on("click", function(){
-	alert("bonjour")
 })
+
+
+//More to the failed gif overlay button, i tried your videos but I a 
+
+//chrome.runtime.onMessage.addListener(
+  //function(request, sender, sendResponse) {
+    //var url = request.message;
+    //$("body").css("background", url)
+
+  //}
+// );
+
+ // TRY: updating the text content of an element or set of elements on one or more pages
+
+// TRY: adding a click event listener to an element that changes the background of that element to a new color
+
 // TRY: using a mouseenter event listener to update text to something new.
---
+++ js/popup.js
@@ -0,0 +1,20 @@
+
+//This is failed popup code that I tried to make work. My goal was to make it so you could click the button and have the gif overlay on the sight, 
+//however, I could not figure out how to make the button work with the gif
+
+
+//console.log("Im the popup!")
+
+//$('#mybutton').on('Click', changeTheme);
+
+//function changeTheme() {
+	//var url = $(this).attr('id');
+
+	//chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	//chrome.tabs.sendMessage(tabs[0].id, {message: url});
+	//});
+//}
+
+//$('#mybutton').on('Click', function(){
+	//$('body').addClass(".noise")
+//})
